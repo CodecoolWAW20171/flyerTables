34e2ae96194c90e4693936d6fd4cf01d64e860e2
Witek <witold.tafel@gmail.com>
2018-10-25 16:04:42 +0200

    changed airports table and added constant_relation and flights table

--- airports.sql
+++ airports.sql
@@ -1,18 +1,14 @@
 create table airports
 (
-  airportid bigserial        not null
+  airport_id varchar          not null
     constraint airports_pkey
     primary key,
-  city      varchar,
-  country   varchar,
-  longitude double precision not null,
-  latitude  double precision not null
+  city       varchar,
+  country    varchar,
+  longitude  double precision not null,
+  latitude   double precision not null
 );
 
-alter table airports
-  owner to witek;
 
 create unique index airports_airportid_uindex
-  on airports (airportid);
-
-
+  on airports (airport_id);
---
+++ flights.sql
@@ -0,0 +1,20 @@
+create table flights
+(
+  flight_id   bigserial not null
+    constraint flights_pkey
+    primary key,
+  relation_id bigint    not null
+    constraint flights_constant_relation__fk
+    references constant_relation,
+  plane_id    bigint    not null,
+  price       money,
+  captain_id  bigint,
+  startdate   timestamp not null,
+  enddate     timestamp not null
+);
+
+
+create unique index flights_flight_id_uindex
+  on flights (flight_id);
+
+
