d7c0fee9b28b43a82fbd3764f9df8946e998a8d3
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-10-25 17:55:54 +0200

    FKeys added

--- all_tables.sql
+++ all_tables.sql
@@ -13,8 +13,6 @@ create table if not exists customers
 	adress varchar
 )
 ;
-
-
 create unique index if not exists customers_customer_id_uindex
 	on customers (customer_id)
 ;
@@ -29,8 +27,6 @@ create table if not exists crew
 	function varchar
 )
 ;
-
-
 create unique index if not exists crew_employee_id_uindex
 	on crew (employee_id)
 ;
@@ -43,8 +39,6 @@ create table if not exists planes
 	seats_amount integer
 )
 ;
-
-
 create unique index if not exists planes_plane_id_uindex
 	on planes (plane_id)
 ;
@@ -60,12 +54,9 @@ create table if not exists passengers
 	phone_number varchar
 )
 ;
-
-
 create unique index if not exists passengers_passenger_id_uindex
 	on passengers (passenger_id)
 ;
-
 create unique index if not exists passengers_email_uindex
 	on passengers (email)
 ;
@@ -81,9 +72,6 @@ create table if not exists airports
 	latitude double precision not null
 )
 ;
-
-
-
 create unique index if not exists airports_airportid_uindex
 	on airports (airport_id)
 ;
@@ -94,13 +82,12 @@ create table if not exists seats
 		constraint seats_pkey
 			primary key,
 	seat_number varchar,
-	plane_id bigint,
+	plane_id bigint
+		constraint planes_plane_id
+			references planes (plane_id),
 	standard varchar
 )
 ;
-
-
-
 create unique index if not exists seats_seat_id_uindex
 	on seats (seat_id)
 ;
@@ -111,18 +98,15 @@ create table if not exists constant_relation
 		constraint constantrelation_pkey
 			primary key,
 	from_airport varchar not null
-		constraint constantrelation_airports_from__fk
-			references airports,
+		constraint constant_relation_airports_from__fk
+			references airports (airport_id),
 	destination_airport varchar not null
 		constraint constant_relation_dest___fk
-			references airports,
+			references airports (airport_id),
 	base_price money,
 	distance integer
 )
 ;
-
-
-
 create unique index if not exists constantrelation_relationid_uindex
 	on constant_relation (relation_id)
 ;
@@ -134,17 +118,16 @@ create table if not exists flights
 			primary key,
 	relation_id bigint not null
 		constraint flights_constant_relation__fk
-			references constant_relation,
+			references constant_relation (relation_id),
 	plane_id bigint not null,
 	price money,
-	captain_id bigint,
+	captain_id bigint
+		constraint crew_employee_id
+			references crew (employee_id),
 	startdate timestamp not null,
 	enddate timestamp not null
 )
 ;
-
-
-
 create unique index if not exists flights_flight_id_uindex
 	on flights (flight_id)
 ;
@@ -155,17 +138,15 @@ create table if not exists flight_seats
 		constraint flightseats_pk
 			primary key
 		constraint flight_seats_seats__fk
-			references seats,
+			references seats (seat_id),
 	passenger_id bigint
 		constraint flight_seats_pesels__fk
-			references passengers,
+			references passengers (passenger_id),
 	flight_id bigint not null
 		constraint flight_seats_flights__fk
-			references flights
+			references flights (flight_id)
 )
 ;
-
-
 
 create table if not exists tickets
 (
@@ -174,20 +155,17 @@ create table if not exists tickets
 			primary key,
 	flight_id bigint not null
 		constraint tickets_flights__fk
-			references flights,
+			references flights (flight_id),
 	seat_id bigint not null
 		constraint tickets_seats__fk
-			references seats,
+			references seats (seat_id),
 	customer_id bigint not null,
 	passenger_id bigint not null
 		constraint tickets_passengers__fk
-			references passengers,
+			references passengers (passenger_id),
 	price money
 )
 ;
-
-
 create unique index if not exists tickets_ticket_id_uindex
 	on tickets (ticket_id)
 ;
-
